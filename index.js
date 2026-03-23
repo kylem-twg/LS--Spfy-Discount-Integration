b9d3a6bf2f488b29e38d542958ab78887bfcae26
kylem-twg <kylem@tailwaggerspets.com>
2026-03-23 11:33:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+// homepage test
 app.get('/', (req, res) => {
   res.send('API is running');
+});
+
+// ✅ THIS IS THE IMPORTANT PART
+app.post('/webhook/discount', (req, res) => {
+  console.log('🔥 WEBHOOK HIT');
+  console.log(JSON.stringify(req.body, null, 2));
+
+  res.sendStatus(200);
 });
 
 app.listen(3000, () => console.log('Server running'));
